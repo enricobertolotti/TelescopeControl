ef90dad43249b4b4866c6b0d2eed25c81fd6abc3
enricobertolotti <hello@enricobertolotti.com>
2020-11-09 15:12:50 +0100

    Initial Commit

---
+++ Telescope Control/Telescope_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Telescope_ControlApp.swift
+//  Telescope Control
+//
+//  Created by Enrico Bertolotti on 09/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Telescope_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
